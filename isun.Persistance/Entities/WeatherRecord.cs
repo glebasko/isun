e5f6e6c16d662b9d2eb69f85f40d0688abb9cbc0
glebas <glebas.kovalenka@gmail.com>
2023-11-27 18:02:49 +0200

    Defined solution structure and added WeatherRecord class

---
+++ isun.Persistance/Entities/WeatherRecord.cs
@@ -0,0 +1,22 @@
+﻿namespace isun.Persistance.Entities
+{
+	public class WeatherRecord
+	{
+		public int Id {  get; set; }
+		public string City { get; set; }
+		public short Temperature { get; set; }
+		public ushort Precipitation { get; set; }
+		public ushort WindSpeed { get; set; }
+		public string Summary { get; set; }
+
+        public DateTime CreatedOn { get; set; }
+    }
+}
+
+//{
+//    "city": "Vilnius",
+//  "temperature": -3,
+//  "precipitation": 11,
+//  "windSpeed": 7,
+//  "summary": "Cool"
+//}
