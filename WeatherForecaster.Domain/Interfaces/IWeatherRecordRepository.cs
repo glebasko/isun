40f4046da1f720dbd2ebf42bf3877e72bd7b8215
glebas <glebas.kovalenka@gmail.com>
2023-11-28 18:53:48 +0200

    - added repository

---
+++ WeatherForecaster.Domain/Interfaces/IWeatherRecordRepository.cs
@@ -0,0 +1,10 @@
+﻿using WeatherForecaster.Domain.DTO;
+
+namespace WeatherForecaster.Domain.Interfaces
+{
+	public interface IWeatherRecordRepository
+	{
+		public Task Add(WeatherRecordDto weatherRecord);
+		public Task AddRange(IEnumerable<WeatherRecordDto> weatherRecords);
+	}
+}
---
+++ WeatherForecaster.Persistance/Repositories/WeatherRecordRepository.cs
@@ -0,0 +1,48 @@
+﻿using WeatherForecaster.Domain.DTO;
+using WeatherForecaster.Domain.Interfaces;
+using WeatherForecaster.Persistance.Entities;
+
+namespace WeatherForecaster.Persistance.Repositories
+{
+	public class WeatherRecordRepository : IWeatherRecordRepository
+	{
+		private readonly WeatherForecasterDbContext _db;
+
+		public WeatherRecordRepository(WeatherForecasterDbContext db)
+		{
+			_db = db;
+		}
+
+		public async Task Add(WeatherRecordDto weatherRecord)
+		{
+			var weatherRecordEntity = new WeatherRecordEntity()
+			{
+				City = weatherRecord.City,
+				Temperature = weatherRecord.Temperature,
+				Precipitation = weatherRecord.Precipitation,
+				WindSpeed = weatherRecord.WindSpeed,
+				Summary = weatherRecord.Summary,
+				CreatedOn = DateTime.Now
+			};
+
+			_db.Add(weatherRecordEntity);
+			await _db.SaveChangesAsync();
+		}
+
+		public async Task AddRange(IEnumerable<WeatherRecordDto> weatherRecords)
+		{
+			var weatherRecordEntities = weatherRecords.Select(item => new WeatherRecordEntity()
+			{
+				City = item.City,
+				Temperature = item.Temperature,
+				Precipitation = item.Precipitation,
+				WindSpeed = item.WindSpeed,
+				Summary = item.Summary,
+				CreatedOn = DateTime.Now
+			});
+
+			await _db.AddRangeAsync(weatherRecordEntities);
+			await _db.SaveChangesAsync();
+		}
+	}
+}
