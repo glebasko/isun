48f562bf70bbe1e4375f5341c02fe4ad7a3f78f3
glebas <glebas.kovalenka@gmail.com>
2023-12-03 13:17:09 +0200

    - refactored program class by moving helper methods to a separate class

---
+++ WeatherForecaster.ConsoleUI/ConsoleHelper.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using WeatherForecaster.Domain.Interfaces;
+using WeatherForecaster.Domain.Services;
+using WeatherForecaster.Infrastructure;
+using WeatherForecaster.Persistance.Repositories;
+using WeatherForecaster.Persistance;
+using Serilog;
+using Microsoft.EntityFrameworkCore;
+
+namespace WeatherForecaster.ConsoleUI
+{
+	public static class ConsoleHelper
+	{
+		// TODO: write tests for this method
+		public static string[] ValidateArgs(string[] cmdArgs, IEnumerable<string> apiCities)
+		{
+			if (cmdArgs.Length == 0)
+			{
+				Console.WriteLine("\nNo cities were provided as arguments. Please restart application with the cities from the list below.");
+				PrintOutCities(apiCities);
+
+				Console.WriteLine("\nExiting the application..");
+				Environment.Exit(1);
+			}
+
+			// eliminate commas 
+			string[] argsWithoutCommas = cmdArgs.Select(s => s.Replace(",", "")).ToArray();
+
+			// eliminate dublicates
+			string[] uniqueArgs = argsWithoutCommas.Distinct().ToArray();
+
+			foreach (var arg in uniqueArgs)
+			{
+				if (!apiCities.Contains(arg))
+				{
+					Console.WriteLine($"\nThere is no weather forecast available for city \"{arg}\".");
+
+					PrintOutCities(apiCities);
+
+					Console.WriteLine("\nPlease run the application with the cities from the list above.");
+					Console.WriteLine("Exiting the application..");
+
+					Environment.Exit(1);
+				}
+			}
+
+			return uniqueArgs;
+		}
+
+		public static ServiceProvider RegisterDependencies()
+		{
+			var services = new ServiceCollection();
+
+			IConfiguration configuration = ConfigurationHelper.BuildConfiguration();
+			services.AddSingleton(configuration);
+
+			services.AddLogging(loggingBuilder =>
+			{
+				loggingBuilder.AddSerilog();
+			});
+
+			string connectionString = configuration.GetConnectionString("TestDatabaseConnection")
+				?? throw new InvalidOperationException("Connection string 'TestDatabaseConnection' not found.");
+
+			string baseApiUrl = configuration["ApiSettings:BaseUrl"]
+				?? throw new InvalidOperationException("Congifuration key 'ApiSettings:BaseUrl' not found.");
+
+			services.AddDbContext<WeatherForecasterDbContext>(options => options.UseSqlServer(connectionString));
+
+			services.AddHttpClient<IWeatherForecastApiService, WeatherForecastApiService>(client =>
+			{
+				client.BaseAddress = new Uri(baseApiUrl);
+			});
+
+			services.AddTransient<IWeatherRecordRepository, WeatherRecordRepository>();
+
+			var serviceProvider = services.BuildServiceProvider();
+
+			return serviceProvider;
+		}
+
+		internal static void PrintOutCities(IEnumerable<string> cities)
+		{
+			if (cities.IsNullOrEmpty())
+			{
+				return;
+			}
+
+			Console.WriteLine("Available cities are: \n");
+
+			foreach (var city in cities)
+			{
+				Console.WriteLine(city);
+			}
+		}
+	}
+}
--- WeatherForecaster.ConsoleUI/Program.cs
+++ WeatherForecaster.ConsoleUI/Program.cs
@@ -1,12 +1,9 @@
 ﻿using WeatherForecaster.Domain.Interfaces;
 using WeatherForecaster.Persistance;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using WeatherForecaster.Infrastructure;
 using Microsoft.Extensions.Configuration;
 using WeatherForecaster.Domain.Services;
-using WeatherForecaster.Persistance.Repositories;
-using Microsoft.IdentityModel.Tokens;
 using Serilog;
 using System.Text;
 
@@ -24,12 +21,12 @@ namespace WeatherForecaster.ConsoleUI
 				Console.WriteLine("Starting the application..");
 				Console.OutputEncoding = Encoding.UTF8; // to ensure that lithuanian letters are shown correctly
 
-				var serviceProvider = RegisterDependencies();
+				var serviceProvider = ConsoleHelper.RegisterDependencies();
 				var weatherForecastApiService = serviceProvider.GetRequiredService<IWeatherForecastApiService>();
 
 				var apiCities = await weatherForecastApiService.GetCitiesAsync();
 
-				string[] uniqueArgs = ValidateArgs(args, apiCities);
+				string[] uniqueArgs = ConsoleHelper.ValidateArgs(args, apiCities);
 
 				var db = serviceProvider.GetRequiredService<WeatherForecasterDbContext>();
 				await DbInitializer.InitializeDbAsync(db);
@@ -75,89 +72,6 @@ namespace WeatherForecaster.ConsoleUI
 				Log.CloseAndFlush();
 
 				Console.WriteLine("\nExiting the application..");
-			}
-		}
-
-		// TODO: write tests for this method
-		private static string[] ValidateArgs(string[] args, IEnumerable<string> apiCities)
-		{
-			if (args.Length == 0)
-			{
-				Console.WriteLine("\nNo cities were provided as arguments. Please restart application with the cities from the list below.");
-				PrintOutCities(apiCities);
-
-				Console.WriteLine("\nExiting the application..");
-				Environment.Exit(1);
-			}
-
-			// eliminate commas 
-			string[] argsWithoutCommas = args.Select(s => s.Replace(",", "")).ToArray();
-
-			// eliminate dublicates
-			string[] uniqueArgs = argsWithoutCommas.Distinct().ToArray();
-
-			foreach (var arg in uniqueArgs)
-			{
-				if (!apiCities.Contains(arg))
-				{
-					Console.WriteLine($"\nThere is no weather forecast available for city \"{arg}\".");
-
-					PrintOutCities(apiCities);
-
-					Console.WriteLine("\nPlease run the application with the cities from the list above.");
-					Console.WriteLine("Exiting the application..");
-
-					Environment.Exit(1);
-				}
-			}
-
-			return uniqueArgs;
-		}
-
-		private static ServiceProvider RegisterDependencies()
-		{
-			var services = new ServiceCollection();
-
-			IConfiguration configuration = ConfigurationHelper.BuildConfiguration();
-			services.AddSingleton(configuration);
-
-			services.AddLogging(loggingBuilder =>
-			{
-				loggingBuilder.AddSerilog();
-			});
-
-			string connectionString = configuration.GetConnectionString("TestDatabaseConnection")
-				?? throw new InvalidOperationException("Connection string 'TestDatabaseConnection' not found.");
-
-			string baseApiUrl = configuration["ApiSettings:BaseUrl"]
-				?? throw new InvalidOperationException("Congifuration key 'ApiSettings:BaseUrl' not found.");
-
-			services.AddDbContext<WeatherForecasterDbContext>(options => options.UseSqlServer(connectionString));
-
-			services.AddHttpClient<IWeatherForecastApiService, WeatherForecastApiService>(client =>
-				{
-					client.BaseAddress = new Uri(baseApiUrl);
-				});
-
-			services.AddTransient<IWeatherRecordRepository, WeatherRecordRepository>();
-
-			var serviceProvider = services.BuildServiceProvider();
-
-			return serviceProvider;
-		}
-
-		private static void PrintOutCities (IEnumerable<string> cities)
-		{
-			if (cities.IsNullOrEmpty())
-			{
-				return;
-			}
-
-			Console.WriteLine("Available cities are: \n");
-
-			foreach (var city in cities)
-			{
-				Console.WriteLine(city);
 			}
 		}
 	}
